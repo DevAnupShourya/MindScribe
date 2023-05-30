c051860539c5eebee1b2406394341460ab82b8f0
DevAnupShourya <contact.yourwebdev@gmail.com>
2023-05-30 18:42:46 +0530

    v1 done

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       colors: {
         // Dark Mode
-        primary:'#26212e',
+        primary: '#26212e',
         secondary: '#1e1d22',
         accent: {
           DEFAULT: '#4341e8', // Links
-          hover: '#31307e',      
+          hover: '#31307e',
         },
         text: '#fdfcff', // Heading
         placeholder: '#78797d',  // Noral Text
@@ -17,5 +17,18 @@ module.exports = {
       },
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require("daisyui"),
+    require('flowbite/plugin')
+  ],
+  daisyui: {
+    themes: ["light", "dark", "synthwave", "cyberpunk", "halloween", "aqua", "luxury", "dracula", "night", "winter"],
+    styled: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
 }
